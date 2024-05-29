9a6b782bb1fc0de4929ab1ecb4a4f831585b63f7
Seokju Yun <wsz871@gmail.com>
2024-05-30 02:25:41 +0900

    Update coco_detection.py

--- downstream/configs/_base_/datasets/coco_detection.py
+++ downstream/configs/_base_/datasets/coco_detection.py
@@ -28,8 +28,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=8,
-    workers_per_gpu=8,
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2017.json',
